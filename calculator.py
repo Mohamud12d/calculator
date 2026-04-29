3b83ad2d08b384cef162c50301b53bdf92495661
mohamed <mohamedshehta08@gmail.com>
2026-04-30 00:36:04 +0300

    add function divison

--- calculator.py
+++ calculator.py
@@ -8,13 +8,18 @@ def subtract(a,b):
     return a-b
 def multiply(a,b):
     return a*b
+def division(a,b):
+    if b==0:
+        return "zero division errors"
+    return a/b
 print("---------Welcome to simple calculator----------")
 def menu():
     while True:
         print("1.add")
         print("2.subtract")
         print("3.multiply")
-        print("4.--exit--")
+        print("4.division")
+        print("5.--exit--")
         a=int(input("enter the number: "))
         b=int(input("enter the number: "))
         choice=int(input("choice: "))
@@ -23,8 +28,10 @@ def menu():
         elif choice==2:
             print(f"{a}-{b}={subtract(a,b)}")  
         elif choice==3:
-            print(f"{a}*{b}={multiply(a,b)}")  
+            print(f"{a}*{b}={multiply(a,b)}") 
         elif choice==4:
+            print(f"{a}/{b}={division(a,b)}") 
+        elif choice==5:
             print("GOODBY")
             break
         else:
